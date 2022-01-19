080659840b6634bfda7a8b12c53625d322c898fc
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2022-01-19 10:56:39 +0530

    Update carbon.py

--- szbot/plugins/carbon.py
+++ szbot/plugins/carbon.py
@@ -26,7 +26,6 @@ TEXT=f"""
 ☘️ **carbon Created Successfully** ✅
 ◇───────────────◇
 🔥 **Created by** : @Hasindu_Image_Tool_BOT
-🌷 **Requestor** : {message.from_user.mention}
 ◇───────────────◇
 
 [🎧 Støck Mυѕιc 🎧](https://t.me/SLStockMusic)
